d75798326a181084f4fda3336c7aa8b260c03c45
zlw-qa <asdori95@gmail.com>
2021-11-16 19:03:36 +0400

    added constructor, include static class and getters\setters

--- src/main/java/com/github/zlwqa/Book.java
+++ src/main/java/com/github/zlwqa/Book.java
@@ -1,21 +1,93 @@
 package com.github.zlwqa;
 
 public class Book {
+    public static String commonName = "Книга";
+    private String nameBook;
+    private String nameAuthor;
+    private int totalCountOfPages; // Общее кол-во страниц
+    private int countOfPagesRead; // Кол-во прочитанных страниц
+    Cover cover;
 
-    String nameBook;
-    int totalCountOfPages; // Общее кол-во страниц
-    int countOfPagesRead; // Кол-во прочитанных страниц
+    public Book(String nameBook, String nameAuthor, int totalCountOfPages, int countOfPagesRead) {
+        this.nameAuthor = nameAuthor;
+        this.nameBook = nameBook;
+        this.totalCountOfPages = totalCountOfPages;
+        this.countOfPagesRead = countOfPagesRead;
+
+    }
+
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public void setNameAuthor(String nameAuthor) {
+        this.nameAuthor = nameAuthor;
+    }
+
+    public int getTotalCountOfPages() {
+        return totalCountOfPages;
+    }
+
+    public void setTotalCountOfPages(int totalCountOfPages) {
+        if (totalCountOfPages > 0) {
+            this.totalCountOfPages = totalCountOfPages;
+        } else {
+            System.out.println("В книге должно быть больше чем 0 страниц\n");
+        }
+    }
+
+    public int getCountOfPagesRead() {
+        return countOfPagesRead;
+    }
+
+    public void setCountOfPagesRead(int countOfPagesRead) {
+        if (countOfPagesRead >= 0 && countOfPagesRead <= totalCountOfPages) {
+            this.countOfPagesRead = countOfPagesRead;
+        } else {
+            System.out.println("Общее кол-во прочитанных страниц должно быть положительным числом " +
+                    "\nи меньше или равно кол-ву всех страниц в книге\n");
+        }
+    }
 
     public void sayNameBook() {
-        System.out.println(nameBook);
+        System.out.println("Название книги: " + nameBook + " ");
+    }
+
+    public void sayNameAuthor() {
+        System.out.println("Имя автора: " + nameAuthor);
+    }
+
+    public static void sayClassName() {
+        System.out.println(Book.class.getName());
     }
 
     public void readToLeft() {
         int countOfPagesRemaining = totalCountOfPages - countOfPagesRead;
         if (countOfPagesRemaining == 0) {
-            System.out.println("Поздравляем книга " + nameBook + " прочитана!");
+            System.out.println("Поздравляем книга " + nameBook + " прочитана!\n");
         } else {
-            System.out.println("В книге " + nameBook + " осталось прочитать " + countOfPagesRemaining + " страниц");
+            System.out.println("В книге " + nameBook + " осталось прочитать " + countOfPagesRemaining + " страниц\n");
+        }
+    }
+
+    static class Cover {
+        String color;
+
+        public Cover(String color) {
+            this.color = color;
+        }
+
+        public void sayColorCover() {
+            System.out.println("Цвет обложки: " + color);
         }
     }
 }
--- src/main/java/com/github/zlwqa/Main.java
+++ src/main/java/com/github/zlwqa/Main.java
@@ -1,21 +1,29 @@
 package com.github.zlwqa;
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
+        Book.sayClassName();// Для обучения
+        System.out.println(Book.commonName); // Для обучения
 
-        Book lagom = new Book();
-        lagom.nameBook = "Lagom";
-        lagom.totalCountOfPages = 200;
-        lagom.countOfPagesRead = 200;
+        Book lagom = new Book("Lagom", "Карлссон Элизабет", 200, 10);
+        lagom.cover = new Book.Cover("Голубой");
+        lagom.cover.sayColorCover();
         lagom.sayNameBook();
+        lagom.sayNameAuthor();
+        lagom.readToLeft();
+        lagom.setCountOfPagesRead(200);
         lagom.readToLeft();
 
-        Book uve = new Book();
-        uve.nameBook = "Uve";
-        uve.totalCountOfPages = 490;
-        uve.countOfPagesRead = 380;
+        lagom.setTotalCountOfPages(0); // Проверка else у setTotalCountOfPages
+        lagom.setCountOfPagesRead(-1);//  Проверка else у setCountOfPagesRead
+
+        Book uve = new Book("Uve","Фредрик Бакман", 490, 380);
+        uve.cover = new Book.Cover("Серый");
+        uve.cover.sayColorCover();
         uve.sayNameBook();
+        uve.sayNameAuthor();
+        uve.readToLeft();
+        uve.setTotalCountOfPages(380);
         uve.readToLeft();
     }
-
 }
